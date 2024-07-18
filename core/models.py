d2764b82122ad14a506c6aaa8db4ef4bf9addc31
Daniel Herculano <dherculano93@gmail.com>
2024-07-18 16:06:58 -0300

    subindo metodos de pesquisa

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    date_creation = models.DateTimeField(auto_now=True)
+    date_event = models.DateTimeField(verbose_name= 'data do evento')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'evento'
